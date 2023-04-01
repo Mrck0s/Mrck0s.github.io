f76d61fbd3ee39d818490b15511eaa86e5132aef
Mrck0s <110942627+Mrck0s@users.noreply.github.com>
2023-04-01 20:06:15 -0300

    css js
    css responsivo terminado, menu desplegable integrado

--- js/nav.js
+++ js/nav.js
@@ -15,3 +15,16 @@ navLinks.forEach(link => {
     });
   });
 });
+
+/*Codigo del menu hamburguesa para dispositivos móviles*/
+const abrir = document.querySelector('.abrir-menu');
+const cerrar = document.querySelector('.cerrar-menu');
+const nav = document.querySelector('.nav');
+
+abrir.addEventListener('click', () => {
+  nav.classList.add('visible');
+});
+
+cerrar.addEventListener('click', () => {
+  nav.classList.remove('visible');
+});
