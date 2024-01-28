1d684da638db3afb2d7cbae782fadbeb7b4768aa
Marcos Furco <110942627+Mrck0s@users.noreply.github.com>
2024-01-27 21:54:04 -0300

    nav header
    nav terminado, falta animacion header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,28 @@
+import { headerData } from "../data/data"
+
+
 export const Header = () => {
     return (
-        <div>Header</div>
+        <header>
+                {
+                    headerData.map(({perfilImg, intro, name, profession}) => {
+                        return (
+                            <div className="container header-container">
+                                <div className="perfil-img">
+                                    <img src={perfilImg} alt="Marcos furco" />
+                                </div>
+                                <div className="header-r">
+                                    <div className="intro">
+                                        <h1>{intro} <span>{name}</span> </h1>
+                                    </div>
+                                    <h5 className="prof">{profession}</h5>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            
+        </header>
 
     )
 }
---
+++ src/data/data.js
@@ -0,0 +1,12 @@
+import perfilImg from '../assets/perfil.png'
+
+
+export const headerData = [
+    {
+        id: 1,
+        perfilImg: perfilImg,
+        intro: "Hi, my name is ",
+        name: "Marcos Furco",
+        profession: "Full-Stack Developer"
+    }
+]
