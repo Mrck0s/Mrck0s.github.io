48c26d3fab48c846672a8241758d5199d2facbab
Marcos Furco <110942627+Mrck0s@users.noreply.github.com>
2024-01-28 11:22:58 -0300

    about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,21 @@
+import { aboutData } from "../data/data"
+
 export const About = () => {
     return (
-        <div>About</div>
+        <section>
+            {
+                aboutData.map(({infoText, link, linkText, aboutText}) => {
+                    return(
+                        <div className="container about-container">
+                            <h3>{infoText} <a href={link}>{linkText}</a> </h3>
+                            <div>
+                                <p>{aboutText}</p>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </section>
 
     )
 }
--- src/data/data.js
+++ src/data/data.js
@@ -10,3 +10,13 @@ export const headerData = [
         profession: "Full-Stack Developer"
     }
 ]
+
+export const aboutData = [
+    {
+        id:1,
+        infoText: 'Currently reworking my project ',
+        link: "https://github.com/Mrck0s/Proyecto_Coompitas",
+        linkText: "Coompitas",
+        aboutText: "A dynamic Full Stack Web Developer who embarked on a self-taught coding journey in 2021. With an insatiable curiosity and a relentless drive to create, I've immersed myself in the ever-evolving world of web development. I'm currently seeking new opportunities and challenges to further enhance my skills and contribute to exciting projects."
+    }
+]
