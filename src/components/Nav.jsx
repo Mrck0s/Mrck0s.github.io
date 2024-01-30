630f3d9099acf7f17c1526624b0b39a98356671e
Marcos Furco <110942627+Mrck0s@users.noreply.github.com>
2024-01-30 19:03:09 -0300

    desktop view finish
    mobile view left

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,6 +11,12 @@ export const Nav = () => {
                 <a href="#" className="logo">
                     <img src={logo} alt="logo" className="logo-img" />
                 </a>
+                <div className="sections-links">
+                    <a href="#about">About</a>
+                    <a href="#experience">Experience</a>
+                    <a href="#projects">Projects</a>
+                    <a href="#contact">Contact</a>
+                </div>
                 <div className="socials">
                     <a href="#"><span><CiInstagram/></span></a>
                     <a href="#"><span><IoLogoGithub/></span></a>
